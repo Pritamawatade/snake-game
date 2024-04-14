078e95d88e085c60295c34bed91c9210a574b5b9
pritamawatade <pritamawatade5@gmail.com>
2024-04-14 08:27:03 +0530

    created the  blueprint of the project

---
+++ javascript/script.js
@@ -0,0 +1,42 @@
+// game function 
+
+
+let direction = {x:0,y:0};
+let foodsound = new Audio('food.wav');
+let gameover = new Audio('gameover.wav');
+let musicSound = new Audio('gameon.wav');
+let movesound = new Audio('move.wav');
+let speed = 2;
+let lastPaintTime = 0;
+let snakearr = [
+    { x:13, y:15}
+]
+
+
+// game function
+
+function main(ctime)
+{
+    window.requestAnimationFrame(main);
+    console.log(ctime);
+    if ((ctime - lastPaintTime)/1000 < 1/speed);
+        return;
+    lastPaintTime = ctime;
+    gameEngine();
+
+}
+
+function gameEngine(){
+    // part1 : updating the snake location
+
+
+    //part2 : display the the location of the snake and its food
+    board.
+}
+
+
+
+
+
+
+window.requestAnimationFrame(main);
